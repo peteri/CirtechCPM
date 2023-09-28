5236377df1d5be8eb759c9d863212fc135642487
Peter Ibbotson <peter@ibbotson.co.uk>
2023-09-28 22:06:07 +0100

    NibReader now writes out the first three tracks.

--- Tools/NibReader/Program.cs
+++ Tools/NibReader/Program.cs
@@ -1,8 +1,4 @@
-﻿
-using System.ComponentModel;
-using System.IO.Enumeration;
-
-namespace NibReader;
+﻿namespace NibReader;
 class Program
 {
     static int[] prodosSectorMap = { 0, 2, 4, 6, 8, 10, 12, 14, 1, 3, 5, 7, 9, 11, 13, 15 };
@@ -11,13 +7,17 @@ class Program
     {
         if (args.Length != 0)
         {
+            // Read in our nibbles
             var tracks = NibReader.ReadTracks(File.ReadAllBytes(args[0]));
+            // Save away the prodos ordered sectors
             var bootList = CreateTrackSectors(0, 0, 2);
             SaveSectors("CPMBoot.bin", tracks, bootList, prodosSectorMap);
             var d000List = CreateTrackSectors(0, 2, 14);
             SaveSectors("D000.bin", tracks, d000List, prodosSectorMap);
             var CpmLdr = CreateTrackSectors(1, 0, 32);
             SaveSectors("CPMLDR.bin", tracks, CpmLdr, prodosSectorMap);
+            // Now do the CPM bits
+
         }
     }
 
@@ -26,7 +26,7 @@ class Program
         var sectors = new List<(int sector, int track)>();
         while (numberOfSectors > 0)
         {
-            sectors.Add((track, sector));
+            sectors.Add((sector: sector, track: track));
             numberOfSectors--;
             sector++;
             if (sector == 16)
@@ -38,18 +38,26 @@ class Program
         return sectors;
     }
 
-    private static void SaveSectors(string filename, List<NibSector>[] nibTracks, IEnumerable<(int sector, int track)> sectors, int[] sectorMap)
+    private static void SaveSectors(
+        string filename,
+        List<NibSector>[] nibTracks,
+        IEnumerable<(int sector, int track)> trackSectors,
+        int[] sectorMap)
     {
-        using (var stream=File.Open(filename,FileMode.Create))
+        Console.Write("Writing to {0} ", filename);
+        using (var stream = File.Open(filename, FileMode.Create))
         {
-            using (var writer=new BinaryWriter(stream))
+            using (var writer = new BinaryWriter(stream))
             {
-                foreach(var sector in sectors)
+                foreach (var ts in trackSectors)
                 {
-                    var mappedSector=sectorMap[sector.sector];
-                    writer.Write(nibTracks[sector.track][mappedSector].Data.Data);
+                    var mappedSector = sectorMap[ts.sector];
+                    Console.Write("(T:{0:X2} S:{1:X} M:{2:X}) ", ts.track, ts.sector, mappedSector);
+                    var sector = nibTracks[ts.track].Where(s => s.Header.Sector == ts.sector).First();
+                    writer.Write(sector.Data.Bytes);
                 }
             }
         }
+        Console.WriteLine(" Done.");
     }
 }
