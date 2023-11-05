04338dfbfab33206d35261cc034aee4b5d1d14a1
Peter Ibbotson <peter@ibbotson.co.uk>
2023-11-05 13:44:08 +0000

    Made D000 compile and compare correctly

--- src/boot/build.ps1
+++ src/boot/build.ps1
@@ -8,13 +8,17 @@ Write-Host 'Building files'
 .\M80.ps1 BOOTSECT
 .\M80.ps1 TOOLKEY
 .\M80.ps1 LDRBIOS
+.\M80.ps1 D000
 Write-Host 'Linking'
 C:\tools\ntvcm.exe ..\..\tools\DRI\LINK BOOTSECT[L0800,NR,NL]=BOOTSECT
 C:\tools\ntvcm.exe ..\..\tools\DRI\LINK TOOLKEY[L0000,NR,NL]=TOOLKEY
+C:\tools\ntvcm.exe ..\..\tools\DRI\LINK D000[LD000,NR,NL]=D000
 C:\tools\ntvcm.exe ..\..\tools\DRI\LINK CPMLDR[L1100,NL]=CPMLDR,LDRBIOS
 Write-Host 'Comparing binaries for BOOTSECT'
 fc.exe /b BOOTSECT.COM BOOTSECT.bin
 Write-Host 'Comparing binaries for TOOLKEY'
 fc.exe /b TOOLKEY.COM TOOLKEY.bin
+Write-Host 'Comparing binaries for D000'
+fc.exe /b D000.COM D000.bin
 Write-Host 'Comparing binaries for CPMLDR'
 fc.exe /b CPMLDR.COM CPMLDR.bin
