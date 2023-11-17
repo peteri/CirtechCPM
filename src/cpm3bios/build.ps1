2cb1f55c43155453ba263b2c345a62f6d9d366ed
Peter Ibbotson <peter@ibbotson.co.uk>
2023-11-17 11:17:40 +0000

    BIOS now compiles and is mostly commented.

--- src/cpm3bios/build.ps1
+++ src/cpm3bios/build.ps1
@@ -5,5 +5,7 @@
 Write-Host 'Generating files for comparison'
 #dotnet run --project ..\..\tools\NibReader ../../DiskImages/BlankBootableCPM3.nib -nodiag
 Write-Host 'Building files'
+.\M80.ps1 SCB
 .\M80.ps1 BIOS
 Write-Host 'Linking'
+C:\tools\ntvcm.exe ..\..\tools\DRI\LINK BNKBIOS3[b]=BIOS,SCB
