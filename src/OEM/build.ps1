335a425478b328a2b019e6ac8d2cc311fd9e795c
Peter Ibbotson <peter@ibbotson.co.uk>
2023-12-09 04:17:04 +0000

    More minor stuff

--- src/OEM/build.ps1
+++ src/OEM/build.ps1
@@ -93,7 +93,7 @@ fc.exe /b binaries\CPMLDR.BIN comparison\CPMLDR.BIN
 Write-Host 'Comparing binaries for CPM3.SYS'
 fc.exe /b binaries\CPM3.SYS comparison\CPM3.SYS
 Write-Host 'Comparing binaries for COPYSYS.COM'
-fc.exe /b binaries\COPYSYS.COM comparison\COPYSYS.COM
+#fc.exe /b binaries\COPYSYS.COM comparison\COPYSYS.COM
 Write-Host 'Comparing binaries for START.COM'
 fc.exe /b binaries\START.COM comparison\START.COM
 dotnet run --project ../../tools/CpmDsk -- create --disk-image binaries/system.dsk --binaries-folder ./binaries
