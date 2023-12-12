95daa23d8ea1cf7577c76d8840bfdf53bec11dfe
Peter Ibbotson <peter@ibbotson.co.uk>
2023-12-12 21:06:45 +0000

    Undoing the BIOSVID patches and started a markdown for the video patches

--- src/patched/buildcom.ps1
+++ src/patched/buildcom.ps1
@@ -1,7 +1,12 @@
 #
 # Helper to build a single utility COM file
+# leaving behind a prn file.
+# use -bin to output a XXX.BIN rather XXX.COM
 #
-param ([string]$filename)
+param (
+	[string]$filename,
+	[switch]$bin
+	)
 C:\tools\ntvcm.exe ..\..\tools\ALDS\M80P.COM =$filename/R/L | Tee-Object -FilePath .\M80ERR.LOG
 $errLog=(Get-Content -Path .\M80ERR.LOG)[-1]
 if ($errLog -ne 'No Fatal Error(s)')
@@ -10,10 +15,15 @@ if ($errLog -ne 'No Fatal Error(s)')
     throw $errMsg
 }
 Remove-Item -Path .\M80ERR.LOG
-$param=$fileName + '[NR,NL]=' + $filename
+$outfile=$filename +'.COM'
+if ($bin -eq $true)
+{
+    $outfile=$filename+".BIN"
+}
+$param=$script:outfile + '[NR,NL]=' + $filename
 C:\tools\ntvcm.exe ..\..\tools\DRI\LINK $param
-Write-Host "Comparing binaries for $filename"
-$params=@('/b', ".`\$fileName.COM", ".`\comparison`\$filename.COM")
+Write-Host "Comparing binaries for $outfile"
+$params=@('/b', ".`\$outfile", ".`\comparison`\$outfile")
 Write-Host $params
 fc.exe $params
 
